cbea2e8b55cef6a296abac3c961b056c4aab7210
MomoHossain <mxh9058@g.rit.edu>
2018-04-12 08:57:44 -0400

    Created three project stacks

--- src/main/java/com/example/appl/IdeaCenter.java
+++ src/main/java/com/example/appl/IdeaCenter.java
@@ -8,35 +8,91 @@ import com.example.model.Idea;
 public class IdeaCenter {
     private static final Logger LOG = Logger.getLogger(IdeaCenter.class.getName());
 
-    private Stack<Idea> encyclopedia;
-    private Stack<Idea> backwardEncyclopedia;
+    private Stack<Idea> pastProjects;
+    private Stack<Idea> presentProjects;
+    private Stack<Idea> futureProjects;
+    private Stack<Idea> backwardStack;
 
     public IdeaCenter(){
-        encyclopedia= new Stack();
-        backwardEncyclopedia= new Stack();
+        pastProjects= new Stack();
+        presentProjects = new Stack();
+        futureProjects= new Stack();
+        backwardStack= new Stack();
         LOG.fine("Idea center created added.");
     }
 
-    public void PushIdea(String name, String description) {
-        encyclopedia.push(new Idea(name,description));
-        LOG.fine("Idea "+name+" added.");
+    public void pushToPast(String name, String description) {
+        pastProjects.push(new Idea(name,description));
+        LOG.fine("Past idea "+name+" added.");
     }
 
-    public Stack<Idea> getEncyclopedia() {
-        return encyclopedia;
+    public void pushToPresent(String name, String description) {
+        presentProjects.push(new Idea(name,description));
+        LOG.fine("Present idea "+name+" added.");
     }
 
-    public Idea getIdea(String name){
-        Stack<Idea> temp = new Stack<>();
+    public void pushToFuture(String name, String description) {
+        futureProjects.push(new Idea(name,description));
+        LOG.fine("Future idea "+name+" added.");
+    }
+
+    public void pushToPast(Idea idea) {
+        pastProjects.push(idea);
+        LOG.fine("Past idea "+idea.getName()+" added.");
+    }
+
+    public void pushToPresent(Idea idea) {
+        presentProjects.push(idea);
+        LOG.fine("Present idea "+idea.getName()+" added.");
+    }
+
+    public void pushToFuture(Idea idea) {
+        futureProjects.push(idea);
+        LOG.fine("Future idea "+idea.getName()+" added.");
+    }
+
+    public Stack<Idea> getPastProjects() {
+        return pastProjects;
+    }
+
+    public Stack<Idea> getPresentProjects() {
+        return presentProjects;
+    }
+
+    public Stack<Idea> getFutureProjects() {
+        return futureProjects;
+    }
+
+    public Idea removeIdeaFromPast(String name){
         Idea output = null;
-        while(!encyclopedia.empty()) {
-            if (encyclopedia.peek().getName().equals(name)) {
-                output = encyclopedia.peek();
-            } else temp.push(encyclopedia.pop());
+        while(!pastProjects.empty()) {
+            if (pastProjects.peek().getName().equals(name)) {
+                output = pastProjects.pop();
+            } else backwardStack.push(pastProjects.pop());
         }
-        while(!temp.empty()){
-            encyclopedia.push(temp.pop());
+        refreshEncyclopedia();
+        return output;
+    }
+
+    public Idea removeIdeaFromPresent(String name){
+        Idea output = null;
+        while(!presentProjects.empty()) {
+            if (presentProjects.peek().getName().equals(name)) {
+                output = presentProjects.pop();
+            } else backwardStack.push(presentProjects.pop());
         }
+        refreshEncyclopedia();
+        return output;
+    }
+
+    public Idea removeIdeaFromFuture(String name){
+        Idea output = null;
+        while(!futureProjects.empty()) {
+            if (futureProjects.peek().getName().equals(name)) {
+                output = futureProjects.pop();
+            } else backwardStack.push(futureProjects.pop());
+        }
+        refreshEncyclopedia();
         return output;
     }
 
@@ -45,15 +101,15 @@ public class IdeaCenter {
     }
 
     public Idea iterate(){
-        if (!encyclopedia.empty()) {
-            backwardEncyclopedia.push(encyclopedia.pop());
-            return backwardEncyclopedia.peek();
+        if (!pastProjects.empty()) {
+            backwardStack.push(pastProjects.pop());
+            return backwardStack.peek();
         } else return null;
     }
 
     public void refreshEncyclopedia(){
-        while (!backwardEncyclopedia.empty()) {
-            encyclopedia.push(backwardEncyclopedia.pop());
+        while (!backwardStack.empty()) {
+            pastProjects.push(backwardStack.pop());
         }
     }
 }
